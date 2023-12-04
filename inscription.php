d8d6685e54eb364d0670a9cd595d58eabf01149e
Asefthukom7 <edensborgelin07@gmail.com>
2023-12-04 17:50:28 +0100

    maj inscription.php 04/12/2023

--- inscription.php
+++ inscription.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, init-ial-scale=1.0">
     <style>@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Licorice&family=Roboto:wght@500&family=Satisfy&family=Shadows+Into+Light&family=Vujahday+Script&display=swap');</style>
     <style>@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Libre+Baskerville:ital@1&family=Licorice&family=Roboto:wght@500&family=Satisfy&family=Shadows+Into+Light&family=Vujahday+Script&display=swap');</style>
-    <link rel="stylesheet" href="Styles/page1_sae203.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <title>INSCRIPTION</title>
 </head>
 <body>
@@ -15,56 +15,109 @@
   
 
     <div id="ins">
-    <form class="box" action="" method="post">
-        <!-- formulaire inscription -->
-    <h1>INSCRIPTION</h1>
+        <form class="box" action="" method="post">
+            <!-- formulaire inscription -->
+            <h1>INSCRIPTION</h1>
 
-    <fieldset class="Civilité">
-        <legend>Civilité</legend>
-        <label for="civilite1">Homme</label>
-        <input type="radio" id="civilite1" name="civilite" value="Homme"/>
-        <label for="civilite1">Femme</label>
-        <input type="radio" id="civilite2" name="civilite" value="femme"/>
-    </fieldset>
+            <fieldset class="Civilité">
+                <legend>Civilité</legend>
+                <label for="civilite1">Homme</label>
+                <input type="radio" id="civilite1" name="civilite" value="Homme"/>
+                <label for="civilite1">Femme</label>
+                <input type="radio" id="civilite2" name="civilite" value="femme"/>
+            </fieldset>
 
-    <fieldset class="NomPrenomDate">
-        <label for="user_name1">Nom :</label>
-        <input type="text" name="user_name1" id="user_name1" placeholder="Nom" required>
+            <fieldset class="NomPrenomDate">
+                <label for="user_name1">Nom :</label>
+                <input type="text" name="user_name1" id="user_name1" placeholder="Nom" required>
 
-        <label for="user_name2">Prénom :</label>
-        <input type="text" name="user_name2" id="user_name2" placeholder="Prénom" required>
+                <label for="user_name2">Prénom :</label>
+                <input type="text" name="user_name2" id="user_name2" placeholder="Prénom" required>
 
-        <label for="user_date">Date de naissance :</label>
-        <input type="date" name="user_date" id="user_date" placeholder="Date de naissance" required>
-    </fieldset>
+                <label for="user_date">Date de naissance :</label>
+                <input type="date" name="user_date" id="user_date" placeholder="Date de naissance" required>
+            </fieldset>
 
-    <fieldset class="adresse">
-          <legend>Adresse</legend>
-          <label>Numero</label>
-          <input id="numRue" name="numRue" type="numRue" /><br />
-          <label>Rue</label>
-          <input id="rue" name="rue" type="text" /><br />
-          <label>Code postal</label>
-          <input id="code-postal" name="code-postal" type="text" /><br />
-          <label>Ville</label>
-          <input id="ville" name="ville" type="text" />
-    </fieldset>
+            <fieldset class="adresse">
+                <legend>Adresse</legend>
+                <label>Numero</label>
+                <input id="numRue" name="numRue" type="numRue" placeholder="N°"/><br />
+                <label>Nom de rue</label>
+                <input id="rue" name="rue" placeholder="Nom de rue" type="text" /><br />
+                <label>Code postal</label>
+                <input id="code-postal" name="code-postal" placeholder="Code postal" type="text" /><br />
+                <label>Ville</label>
+                <input id="ville" name="ville" placeholder="Ville" type="text" />
+            </fieldset>
 
-     
-    <label for="user_email">Email :</label>
-    <input type="email" name="user_email" id="user_email" placeholder="Email" required>
+            <fieldset class="situation">
+                <label for="situationFamiliale">Votre situation familiale :</label>
+                <select id="situationFamiliale" name="situationFamiliale" onchange="toggleDates()" required>
+                    <option value="celibataire">Célibataire</option>
+                    <option value="marie">Marié(e)</option>
+                    <option value="unionLibre">Union Libre</option>
+                    <option value="pacs">PACS</option>
+                    <option value="divorce">Divorcé(e)</option>
+                </select>
+                <label for="dateDebut" id="dateDebutLabel" class="hidden">Date de début :</label>
+                <input type="date" id="dateDebut" name="dateDebut" class="hidden">
 
-    <label for="phone">Téléphone</label>
-    <input type="tel" id="phone" name="phone"/>
-    
-    <input type="submit" name="valider" value="Valider">
-    <p>Déjà <a href="page2_sae203.html">inscrit ?</a></p> <!-- lien page de connexion -->
-</form>
+                <label for="dateFin" id="dateFinLabel" class="hidden">Date de fin :</label>
+                <input type="date" id="dateFin" name="dateFin" class="hidden">
+            </fieldset>
 
-        
+            <fieldset class="charge">
+                <legend>Personne à charge</legend>
+                <label>Enfant mineur</label>
+                <input id="min" name="mineur" type="number" min="0" required/><br />
+                <label>Enfant majeur</label>
+                <input id="maj" name="majeur" type="number"  min="0" required/><br />
+                <label>Parent à charge</label>
+                <input id="parent" name="parent" type="number"  min="0" required/>
+                <label>Cause de handicap</label>
+                <input id="handicap" name="handicap" type="text"  />
+            </fieldset>
+
+            
+            <label for="user_email">Email :</label>
+            <input type="email" name="user_email" id="user_email" placeholder="Email" required>
+
+            <label for="phone">Téléphone</label>
+            <input type="tel" id="phone" name="phone" placeholder="Téléphone" required/>
+            
+            <input type="submit" name="valider" value="Valider">
+            <p>Déjà <a href="connexion.php">inscrit ?</a></p> <!-- lien page de connexion -->
+        </form>
+
     </div>
 
+    <script>
+        function toggleDates() {
+            var situationFamiliale = document.getElementById("situationFamiliale").value;
+            var dateDebutLabel = document.getElementById("dateDebutLabel");
+            var dateDebut = document.getElementById("dateDebut");
+            var dateFinLabel = document.getElementById("dateFinLabel");
+            var dateFin = document.getElementById("dateFin");
 
+            if (situationFamiliale === "celibataire" || situationFamiliale === "unionLibre") {
+                dateDebutLabel.classList.add("hidden");
+                dateDebut.classList.add("hidden");
+                dateFinLabel.classList.add("hidden");
+                dateFin.classList.add("hidden");
+            } else if (situationFamiliale === "marie" || situationFamiliale === "pacs") {
+                dateDebutLabel.classList.remove("hidden");
+                dateDebut.classList.remove("hidden");
+                dateFinLabel.classList.add("hidden");
+                dateFin.classList.add("hidden");
+            } else {
+                dateDebutLabel.classList.remove("hidden");
+                dateDebut.classList.remove("hidden");
+                dateFinLabel.classList.remove("hidden");
+                dateFin.classList.remove("hidden");
+            }
+        }
+
+    </script>
       
 </body>
 </html>
---
+++ widget.php
@@ -0,0 +1,1 @@
+<iframe id="widget_autocomplete_preview"  width="150" height="300" frameborder="0" src="https://meteofrance.com/widget/prevision/772840"> </iframe>
