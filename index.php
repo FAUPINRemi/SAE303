d623eb310fa29ada04c34ce2a1ac6d8614a9b60d
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-01-02 18:50:07 +0100

    navbar ok + debut carrousel

--- index.php
+++ index.php
@@ -8,6 +8,55 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Test</h1>
+<nav class="navbar fixed-top navbar-expand-lg bg-dark bg-opacity-25 text-white">
+  <a class="navbar-brand" href="index.php"><img style="width: 100px; height: 100px;" src="./ACF2L/images/logoaf2l.png" alt="logoaf2l"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse text-white z-3 container" id="navbarSupportedContent">
+      <!-- Left links -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-white p-10 ">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Informations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Réserver</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Contact</a>
+        </li>
+        <!-- Navbar dropdown -->
+        <div class="btn-group">
+  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+  <img style="width: 40px; height: 40px;" src="./ACF2L/images/Log_white_icon.png" alt="logicon">
+  </button>
+  <ul class="dropdown-menu bg-dark bg-opacity-25 container dropdown-menu-end">
+    <li><a class="dropdown-item text-white" href="#">Login</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item text-white" href="#">Consulter réservations</a></li>
+  </ul>
+
+</nav>
+
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="  filter: brightness(65%);" src="./ACF2L/images/slide2.jpg" class="d-block w-100" alt="Slide1">
+      <div class="carousel-caption mx-auto p-2">
+        <h1 class="mx-auto shadow-sm p-1 mb-5 ">Bienvenue dans notre association ! <br>
+Aéro club de Frotey-Les-Lures</h1>
+      
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+</div>
 </body>
 </html>
