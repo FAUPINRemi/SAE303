f3ff22479f6ab33c232bff50c76b4a57d253a4bb
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-01-06 18:27:00 +0100

    modif des indexs car des redirections n'était pas ok

--- inscription.php
+++ inscription.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, init-ial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./ACF2L/images/logoaf2l.png">
     <link rel="stylesheet" href="style.css" type="text/css">
     <title>INSCRIPTION</title>
     
